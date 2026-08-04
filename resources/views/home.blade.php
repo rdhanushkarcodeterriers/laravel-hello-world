2fd40e9226c7cca3db6e6824cebac3789c1fbc1a
rdhanushkarcodeterriers <r.dhanushkar@codeterriers.com>
2026-08-04 15:21:11 +0530

    Initial Laravel Hello World commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel on Azure</title>
+</head>
+<body>
+    <h1>Hello World from Laravel on Azure!</h1>
+    <p>Welcome to my Laravel application.</p>
+</body>
+</html>
